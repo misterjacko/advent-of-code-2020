ed2808212c7d61dbeb58ffefc66b5eb30b9d63fc
Jakob Ondrey <ondrey@gmail.com>
2020-12-03 20:44:02 -0600

    adds tests and combines both parts in days 1 and 2

--- Day1/1.1-Report_Repair.py
+++ Day1/1.1-Report_Repair.py
@@ -1,19 +1,40 @@
 from datetime import datetime
 
-starttime = datetime.now()
-transactions = []
+def readFile(filename):
+    transactions = []
+    datafile = open(filename, "r")
+    for aline in datafile:
+        transactions.append(int(aline))
+    datafile.close()
+    return transactions
 
-datafile = open("day1data.txt", "r")
-for aline in datafile:
-    transactions.append(int(aline))
-datafile.close()
-
-def searchfor2020 (A):
+def part1(A):
   for i in A:
     remainder = 2020 - i
     if remainder in A:
       return (i*remainder)
   return ("No Matches")
 
-print (searchfor2020(transactions))
-print (datetime.now()-starttime)
+#this is brute force but the data set is small so :shrug:
+def part2(A):
+  for i in A:
+    for j in A:
+      remainder = 2020 - j - i
+      if remainder in A:
+        if j in A:
+          return (i*j * remainder)
+  return ("No Matches")
+
+def test():
+  test_input = readFile("test.txt")
+  assert part1(test_input) == 514579
+  assert part2(test_input) == 241861950
+
+if __name__ == "__main__":
+  test()
+  starttime = datetime.now()
+  transactions = readFile("day1data.txt")
+  print(f"Part 1: {part1(transactions)}")
+  print (datetime.now()-starttime)
+  print(f"Part 2: {part2(transactions)}")
+  print (datetime.now()-starttime)
